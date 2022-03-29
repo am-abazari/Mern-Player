a988c2dc56701b0ed207af07b7abc6ddb85d179c
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2022-03-29 20:01:14 +0430

    tailwind added

---
+++ frontend/src/Pages/Musics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Musics = () => {
+    return (
+        <div>
+            <h1 className='text-red-700'>salam</h1>
+        </div>
+    );
+};
+
+export default Musics;
